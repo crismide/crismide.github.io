c47c9395a350b7088513311fae12bdf128e73dad
Cris Milego <crissmilego@gmail.com>
2025-01-21 21:36:20 +0100

    updated webiste

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="bg-[#404040] p-20 text-white w-full">
+        <h3 className="text-xl mb-10">My contact info</h3>
+        <p className="mb-5">crissmilego@gmail.com</p>
+        <a href="https://www.linkedin.com/in/cris-milego-de-diego-30629b153/" target="_blank" className="underline">LinkedIn</a>
+    </footer>
+  )
+}
+
+export default Footer
